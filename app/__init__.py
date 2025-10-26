d938181e264ec146f79aa2d20380e91aba9c9ea8
Gustavo Maximo Da Silva <gmaximodasilva@gmail.com>
2025-10-26 19:48:29 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ def create_app(config_name='default'):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    # Importa as rotas
     from . import routes
     app.register_blueprint(routes.bp)
 
